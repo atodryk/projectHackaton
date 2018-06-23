f58234d4a1862c49befbe1ba23e33e680d4b04f9
Adam Todryk <adam.todryk@gmail.com>
2018-06-23 10:50:09 +0200

    initial v2

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var nunjacksRender = require('gulp-nunjucks-render');
+
+gulp.task('img', function () {
+  gulp.src('./assets/img/**/*')
+    .pipe(gulp.dest('./web/img'))
+});
+
+gulp.task('sass', function () {
+  gulp.src('./assets/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./web/css/'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('js', function(){
+   gulp.src(['assets/js/app.js'])
+    .pipe(gulp.dest('./web/js/'));
+});
+
+gulp.task('media', function() {
+  gulp.src('./assets/media/**/*')
+  .pipe(gulp.dest('./web/media/'))
+})
+
+
+gulp.task('nunjucks', function() {
+  return gulp.src(['assets/templates/*.html'])
+    .pipe(nunjacksRender({
+      path: ['assets/templates']
+    }))
+    .pipe(gulp.dest('./web'))
+});
+
+gulp.task('build', ['img','media', 'sass', 'nunjucks', 'js'], function() {
+    browserSync.init({
+        server: "./web"
+    });
+    gulp.watch("assets/scss/**/*.scss", ['sass']);
+    gulp.watch("assets/templates/**/*.html", ['nunjucks']);
+    gulp.watch("assets/js/*.js", ['js']);
+    gulp.watch("web/*.html").on('change', browserSync.reload);
+    gulp.watch("web/js/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build']);
